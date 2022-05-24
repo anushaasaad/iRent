41d5fa8a236887a2a5189f5a96f9516230569bc4
Anusha Saad <62885127+anushaasaad@users.noreply.github.com>
2022-05-24 22:01:30 +0500

    Update admincar.js

--- Frontend/src/components/Admin/admincar.js
+++ Frontend/src/components/Admin/admincar.js
@@ -5,7 +5,9 @@ import {useState} from 'react';
 import Axios from "axios";
 import Insert from './insert.js';
 import Delete from './delete.js';
+import Details from './details.js';
 import { useHistory } from 'react-router';
+import Header from '../../header.js'
 import {
     BrowserRouter as Router,
     Route,
@@ -16,10 +18,13 @@ import {
 function Adminairbnb() {
     const navigate = useHistory();
     return (
+        <div>
+            <Header />
         <Router>
         <div className="Admin">
             <div className="module">
                             <ul>
+                                <li><Link to="/Admincar/details"><Button>Booking details</Button></Link></li>
                                 <li><Link to="/Admincar/insert"><Button>Insertion</Button></Link></li>
                                 <li><Link to="/Admincar/delete"> <Button>Deletion</Button></Link></li>
         
@@ -29,14 +34,16 @@ function Adminairbnb() {
                             <Route exact path="/Admincar/insert">
                                 <Insert />
                             </Route>
+                            <Route exact path="/Admincar/details">
+                                <Details />
+                            </Route>
                             <Route path="/Admincar/delete">
                                 <Delete />
                             </Route>
                     </Switch>
-
-            
     </div>
     </Router>
+    </div>
     )
 };
 export default Adminairbnb;
